629e897aecf14f15539197f7ecb9c09a477ff487
LeoAboard <leonardoarmelin2003@outlook.com>
2025-08-05 17:14:46 -0400

    Controle de usuário finalizado, CRUD, autenticação e autorização

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,37 +1,158 @@
 const fs = require('fs')
 const { generateToken } = require('../middlewares/auth')
+const { ReadableStreamDefaultController } = require('stream/web')
 
-class userController{
+class userController {
 
-    constructor(con){
+    constructor(con) {
         this.con = con
+
+        this.exibirUsuario = this.exibirUsuario.bind(this)
+        this.saveUser = this.saveUser.bind(this)
+        this.userLogin = this.userLogin.bind(this)
+        this.atualizarUsuario = this.atualizarUsuario.bind(this)
+        this.excluirUsuario = this.excluirUsuario.bind(this)
     }
 
-    exibirUsuario(){
+    exibirUsuario(req, res) {
+        const { nome } = req.body
 
+        const query = `SELECT * FROM usuario WHERE nome = "${nome}"`
+
+        this.con.query(query, function (err, result) {
+            if (err) { res.send(`Erro ao exibir: ${err}`) }
+            else { res.send(result) }
+        })
     }
 
-    saveUser(req, res){
-   
+    async saveUser(req, res) {
+        const { nome, email, senha } = req.body
+
+        let date = new Date()
+        date = date.toISOString()
+        date = date.slice(0, 19)
+
+        const aut_query = `SELECT email FROM usuario WHERE email = "${email}"`
+
+        let resultado = await new Promise((resolve, reject) => {
+            this.con.query(aut_query, (err, result) => {
+                if(err) return reject(err);
+                return resolve(result)
+            })
+        })
+
+        if(resultado[0] && resultado[0].email == email){
+            res.send("Usuário já cadastrado")
+            return
+        }
+
+        const query = `INSERT INTO usuario (nome, email, senha, data_cadastro, data_atualizacao) VALUES ("${nome}", "${email}", "${senha}", "${date}", "${date}")`
+        this.con.query(query, function (err) {
+            if (err) { res.send(`Erro ao inserir: ${err}`) }
+            else { res.send(`Usuário ${nome} cadastrado com sucesso!`) }
+        })
+
+        return
     }
 
-    userLogin(req, res){
+    async userLogin(req, res) {
+        const { email, senha } = req.body
+        const aut_query = `SELECT id, nome, email, senha FROM usuario WHERE email = "${email}" AND senha = "${senha}"`
 
-        const token = generateToken(id)
+        let resultado = await new Promise((resolve, reject) => {
+            this.con.query(aut_query, (err, result) => {
+                if(err) return reject(err);
+                return resolve(result)
+            })
+        })
 
-        res.cookie('token', token, {              //envio do token para o cliente
+        if(!resultado[0]){
+            res.send("Email ou senha incorretos")
+            return
+        }
+
+        const token = generateToken(resultado[0].id)
+
+        res.cookie('token', token, {                  //envio do token para o cliente
             httpOnly: true,
             secure: false,
-            maxAge: 5*24*60*60*1000
-        })  
+            maxAge: 5 * 24 * 60 * 60 * 1000
+        })
+
+        res.send(`Seja bem vindo ${resultado[0].nome}`)
+        return
     }
 
-    atualizarUsuario(){
+    async atualizarUsuario(req, res) {
+        const { novoNome, novoEmail, novaSenha } = req.body
 
+        let date = new Date()
+        date = date.toISOString()
+        date = date.slice(0, 19)
+
+        const id = req.id.id
+
+        if(novoNome){
+            const query = `UPDATE usuario SET nome = "${novoNome}", data_atualizacao = "${date}" WHERE id = ${id};`
+
+            this.con.query(query, function(err){
+                if(err) { res.send(`Erro ao atualizar: ${err}`)}
+                else{res.send(`Nome atualizado com sucesso: ${novoNome}!`)}
+            })
+
+            return
+        }
+
+        if(novaSenha){
+            const query = `UPDATE usuario SET senha = "${novaSenha}", data_atualizacao = "${date}" WHERE id = ${id};`
+
+            this.con.query(query, function(err){
+                if(err) { res.send(`Erro ao atualizar: ${err}`)}
+                else{res.send(`Senha atualizada com sucesso!`)}
+            })
+
+            return
+        }
+
+        if(novoEmail){
+
+            const aut_query = `SELECT email FROM usuario WHERE email = "${novoEmail}"`
+            let resultado = await new Promise((resolve, reject) => {
+                this.con.query(aut_query, (err, result) => {
+                    if(err) return reject(err);
+                    return resolve(result)
+                })
+            })
+            if(resultado[0] && resultado[0].email == novoEmail){
+                res.send("Este email já está em uso.")
+                return
+        }
+
+            const query = `UPDATE usuario SET email = "${novoEmail}", data_atualizacao = "${date}" WHERE id = ${id};`
+            this.con.query(query, function(err){
+                if(err) { res.send(`Erro ao atualizar: ${err}`)}
+                else{res.send(`Email atualizado com sucesso: ${novoEmail}`)}
+            })
+
+            return
+        }
+
+        res.send("Erro: Impossivel atualizar este dado")
+        return
     }
 
-    excluirUsuario(){
+    excluirUsuario(req, res) {
+        const { email, senha } = req.body
 
+        const query = `DELETE FROM usuario WHERE email = "${email}" AND senha = "${senha}"`
+        this.con.query(query, function(err){
+            if(err) { res.send(`Erro ao excluir: ${err}`)}
+            else{res.send(`Sua conta foi excluida.`)}
+        })
+
+        res.cookie('token', '', {expires: new Date(0)})
+
+        return
     }
 }
 
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,4 +1,5 @@
 const userController = require('../controllers/UserController')
+const { authToken } = require('../middlewares/auth')
 const con = require('../models/connection')
 
 MyUserController = new userController(con)
@@ -7,8 +8,8 @@ const userRoutes = (app) => {
     app.post('/signup', MyUserController.saveUser)
     app.post('/signin', MyUserController.userLogin)
     app.get('/usuario', MyUserController.exibirUsuario)
-    app.put('/usuario', MyUserController.atualizarUsuario)
-    app.delete('/usuario', MyUserController.excluirUsuario)
+    app.put('/usuario', authToken, MyUserController.atualizarUsuario)
+    app.delete('/usuario', authToken, MyUserController.excluirUsuario)
 }
 
 module.exports = userRoutes
